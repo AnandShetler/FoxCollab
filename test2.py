caa2846a7b39a6c080ca0d86b70d59578f2edcbb
CelinaPeralta <celper19@bergen.org>
2020-02-22 18:05:29 -0500

    ha

--- test2.py
+++ test2.py
@@ -2,59 +2,80 @@ from psonic import *
 from threading import Thread
 from random import choice
 
-
 SCALES = {0: MAJOR,
-          1: MINOR,
-          2: MAJOR_PENTATONIC,
-          3: MINOR_PENTATONIC,
-          4: MELODIC_MAJOR,
-          5: MELODIC_MINOR,
-          6: MAJOR7,
-          7: DORIAN,
+          1: MAJOR_PENTATONIC,
+          2: MELODIC_MAJOR,
+          3: MAJOR7,
+          4: DORIAN,
+          5: MINOR,
+          6: MINOR_PENTATONIC,
+          7: MELODIC_MINOR,
           8: MIXOLYDIAN,
           9: IONIAN,
           10: AEOLIAN}
 
-KEYS = {0: C5,
-        1: D5,
-        2: E5,
-        3: F5,
-        4: G5,
-        5: A5,
-        6: Bb5,
-        7: B5}
+KEYS = {0: C4,
+        1: D4,
+        2: E4,
+        3: F4,
+        4: G4,
+        5: A4,
+        6: Bb4,
+        7: B4}
 
-bpm = 50
+bpm = 120
+scaled_bpm = 120
 sc = SCALES[0]
 rhythm = 10
 mood = 10
 key = KEYS[0]
+scaled_key = key
 
 full_scale = scale(key, sc)
 
 query = "b 120"
 
 
-def my_loop(b, s, r, m, k):
+def drum_loop(b, s, r, m, k):
+    if one_in(2):
+        sample(DRUM_HEAVY_KICK)
+        # sleep(60.0 / b)
+    else:
+        if one_in(2):
+            sample(DRUM_SNARE_HARD)
+    sample(DRUM_CYMBAL_CLOSED)
+    sleep(30.0 / b)
+
+
+def lead_loop(b, s, r, m, k):
     n = random.choice(scale(k, s))
-    re = random.choice([0.5, 0.25, 1, 2])
+    re = random.choice([1])
     play(n, release=re)
     sleep(60.0 / b)
 
-def looper():
-  while True:
-    my_loop(bpm, sc, rhythm, mood, key)
 
-looper_thread = Thread(name='looper', target=looper)
+def lead_looper():
+    while True:
+        lead_loop(scaled_bpm, sc, rhythm, mood, key)
+
+
+def drum_looper():
+    while True:
+        drum_loop(scaled_bpm, sc, rhythm, mood, key)
+
+
+looper_thread = Thread(name='lead_looper', target=lead_looper)
+drum_thread = Thread(name='drum_looper', target=drum_looper)
 
 looper_thread.start()
+drum_thread.start()
 
 while query != "end":
     x = query.split(" ")
 
     if len(x) == 2:
         param = x[0]
-        val = int(x[1])
+        val = float(x[1])
 
         if param in "bsrmk":
 
@@ -73,6 +94,10 @@ while query != "end":
                     key = KEYS[int(val)]
                     full_scale = scale(key, sc)
 
+            scaled_bpm = bpm + (10 * (mood - 5))
+            scaled_key = 0
+
+
+
+
     query = input()
-
-
